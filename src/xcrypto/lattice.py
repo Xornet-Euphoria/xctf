cd78fa03e634d32f6d370c7c18e30d155f849303
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-09-17 18:24:02 +0900

    クソデカい数字でのgaussian_reductionが失敗する問題を修正

--- src/xcrypto/lattice.py
+++ src/xcrypto/lattice.py
@@ -4,10 +4,10 @@ from xcrypto.vector import dot_product
 
 def gaussian_reduction(v1, v2):
     while True:
-        if v2.norm() < v1.norm():
+        if dot_product(v2, v2) < dot_product(v1, v1):
             v1, v2 = v2, v1
 
-        m = floor(dot_product(v1, v2) / dot_product(v1, v1))
+        m = dot_product(v1, v2) // dot_product(v1, v1)
         if m == 0:
             return v1, v2
 
