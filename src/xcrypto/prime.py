7f793ab0a32f70619a1d0fd380cd913d63c1d8e6
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-12-18 14:36:14 +0900

    素因数分解

--- src/xcrypto/prime.py
+++ src/xcrypto/prime.py
@@ -1,4 +1,5 @@
 from xcrypto.num_util import *
+from math import sqrt, floor
 from Crypto.Util.number import isPrime
 
 
@@ -32,3 +33,68 @@ def fermat_method(n):
     b = int_sqrt(b_pow_2)
 
     return (a + b, a - b)
+
+
+"""
+below codes are not for CTF
+"""
+
+
+def create_sieve(n):
+    sieve = [True for _ in range(n + 1)]
+    sieve[0] = False
+    sieve[1] = False
+    for i in range(2, floor(sqrt(n))):
+        if not sieve[i]:
+            continue
+
+        for j in range(i**2, n+1, i):
+            sieve[j] = False
+
+    return sieve
+
+
+def create_spf_sieve(n):
+    sieve = [i for i in range(n + 1)]
+    for i in range(2, floor(sqrt(n))):
+        if sieve[i] != i:
+            continue
+
+        for j in range(i**2, n+1, i):
+            if sieve[j] == j:
+                sieve[j] = i
+
+    return sieve
+
+
+def __factorize_submodule(n, sieve):
+    ret = {}
+    if n == 0:
+        ret[0] = 0
+        return
+    if n == 1:
+        ret[1] = 1
+        return
+    while n != 1:
+        spf = sieve[n]
+        n //= spf
+        if not spf in ret:
+            ret[spf] = 0
+
+        ret[spf] += 1
+
+    return ret
+
+
+def factorize(n):
+    sieve = create_spf_sieve(n)
+    return __factorize_submodule(n, sieve)
+
+
+def all_factorize(n):
+    sieve = create_spf_sieve(n)
+    ret = [{} for _ in range(n + 1)]
+    for i in range(n+1):
+        ret[i] = __factorize_submodule(i, sieve)
+
+    return ret
