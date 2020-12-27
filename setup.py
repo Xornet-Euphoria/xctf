566b74ed06379711d422d9409c09179568f6c6c0
Xornet-Euphoria <einnotwendigesubel@gmail.com>
2020-12-27 17:31:25 +0900

    requirementを記述

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="xctf",
     version="0.1",
-    install_requires=[],
+    install_requires=["pycryptodome", "factordb-python"],
     package_dir={"": "src"},
     packages=find_packages("src"),
     scripts=["scripts/make_exploit.py"]
